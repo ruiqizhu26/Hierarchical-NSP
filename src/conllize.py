6b84f5bee32c1220873538df6d215475cc3507db
ruiqizhu26 <45186031+ruiqizhu26@users.noreply.github.com>
2021-10-27 15:31:19 -0400

    Oct 27 checkpoint

--- src/conllize.py
+++ src/conllize.py
@@ -10,12 +10,7 @@ class WiFiNECoNLLizer:
 
         self.document_vocab = {}
         self.figer_vocab = {}
-        self.segments = []
-        self.sentences = []
-
-        self.art_id = None
-        self.sen_idx_min = None
-        self.sen_idx_max = None
+        self.art2doc = {} # will be populated by load_document_paths()
 
         self.load_document_vocab()
         self.load_figer_vocab()
@@ -49,27 +44,45 @@ class WiFiNECoNLLizer:
         print('time for load_figer_vocab():', round(time.time() - start, 2), 'seconds')
 
 
-    """
-    Find and load the document file that contains the article with self.art_id 
-    """
-    # TODO Oct 25, 2021: This function seems to be the bottleneck (about 30s for 1000+ annotation indices)
-    def load_document_path(self):
+    # """
+    # Find and load the document file that contains the article with self.art_id 
+    # """
+    # # TODO Oct 25, 2021: This function seems to be the bottleneck (about 30s for 1000+ annotation indices)
+    # def load_document_path(self):
+    #     start = time.time()
+    #     search_path = PREFIX + 'Documents/'
+    #     i = 0
+    #     while True:
+    #         try:
+    #             with open(search_path + str(i), 'r') as file:
+    #                 for line in file:
+    #                     line = line.split()
+    #                     if line[0] == 'ID':
+    #                         if int(line[1]) == self.art_id:
+    #                             self.document_path = search_path + str(i)
+    #                             print('time for load_document_path():', round(time.time() - start, 2), 'seconds')
+    #                             return 
+    #         except:
+    #             pass
+    #         i += 1
+
+
+    def load_document_paths(self):
         start = time.time()
         search_path = PREFIX + 'Documents/'
         i = 0
-        while True:
+        while i <= NUM_FILES:
             try:
                 with open(search_path + str(i), 'r') as file:
                     for line in file:
                         line = line.split()
                         if line[0] == 'ID':
-                            if int(line[1]) == self.art_id:
-                                self.document_path = search_path + str(i)
-                                print('time for load_document_path():', round(time.time() - start, 2), 'seconds')
-                                return 
+                            art_id = int(line[1])
+                            self.art2doc[art_id] = search_path + str(i)
             except:
                 pass
             i += 1
+        print('time for load_document_paths():', round(time.time() - start, 2), 'seconds', '\n')
 
 
     """
@@ -77,75 +90,71 @@ class WiFiNECoNLLizer:
     Result: Generate a dataset in text file format similar to CoNLL-2003
     format: [sentIdx, begin, end, menType, figer_types, gillick_types]
     """
-    def conllize(self):
+    def conllize_article(self, annotation_path, output_path, art_id, sen_idx_min, sen_idx_max):
+        # print('annotation_path:', annotation_path)
+        # print('output_path:', output_path)
+        # print('art_id:', art_id)
+        # print('sen_idx_min:', sen_idx_min)
+        # print('sen_idx_max:', sen_idx_max)
+        # if (art_id != 432236):
+        #     return
         start = time.time()
-        # Read figer annotations
-        with open(self.annotation_path, 'r') as file:
-            not_first = False
+        # Read figer annotations]
+        segments = [None] * (sen_idx_max - sen_idx_min + 1) # don't do self.segments = [[]] * 66
+        for i in range(len(segments)):
+            segments[i] = []
+
+        with open(annotation_path, 'r') as file:
+            found = False
             for line in file:
                 line = line.split()
                 if (line[0] == 'ID'):
-                    if not_first: break # only parse the first article
-                    self.art_id = int(line[1])
-                    not_first = True
-                    continue
-                sen_idx = int(line[0])
+                    if found: 
+                        break # only parse the target article (since there the article)
+                    if (int(line[1]) == art_id):
+                        found = True
+                elif found:
+                    try:
+                        sen_idx = int(line[0]); begin = int(line[1]); end = int(line[2]) - 1 # it looks like the end is exclusive
+                        men_type = int(line[3]); figer_type = self.figer_vocab[int(line[4])]
+                        # print('sen_idx:', sen_idx)
+                    except: # the line is malformed
+                        print('malformed...')
+                        continue
+                    # self.segments[i] is a list of tuple; each tuple corresponds to a segment in sentence i
+                    if men_type == 0: # means it's a named entity
+                        segments[sen_idx - sen_idx_min].append([begin, end, figer_type]) # occasionally in the annotation file an article starts at sentence 1 for some reason
 
-                if self.sen_idx_min is None: self.sen_idx_min = sen_idx
-                self.sen_idx_max = sen_idx
-
-        self.segments = [None] * (self.sen_idx_max - self.sen_idx_min + 1) # don't do self.segments = [[]] * 66
-        for i in range(len(self.segments)):
-            self.segments[i] = []
-
-        with open(self.annotation_path, 'r') as file:
-            not_first = False
-            for line in file:
-                line = line.split()
-                if (line[0] == 'ID'):
-                    if not_first: break # only parse the first article
-                    self.art_id = int(line[1])
-                    not_first = True
-                    continue
-                try:
-                    sen_idx = int(line[0]); begin = int(line[1]); end = int(line[2]) - 1 # it looks like the end is exclusive
-                    men_type = int(line[3]); figer_type = self.figer_vocab[int(line[4])]
-                except: # the line is malformed
-                    continue
-                # self.segments[i] is a list of tuple; each tuple corresponds to a segment in sentence i
-                if men_type == 0: # means it's a named entity
-                    self.segments[sen_idx].append([begin, end, figer_type])
-
-        self.load_document_path() # find document with correct article id
-
+        document_path = self.art2doc[art_id]
+        sentences = []
         # Read document data
-        with open(self.document_path, 'r') as file:
+        with open(document_path, 'r') as file:
             sen_idx = -1
             right_article = False
             for i, line in enumerate(file):
                 line = line.split()
                 if line[0] == 'ID':
-                    if int(line[1]) == self.art_id: 
+                    if int(line[1]) == art_id: 
                         right_article = True
                         continue
                     elif right_article:
                         break
                 if right_article:
                     sen_idx += 1
-                    if sen_idx >= self.sen_idx_min and sen_idx <= self.sen_idx_max:
-                        self.sentences.append([])
+                    if sen_idx >= sen_idx_min and sen_idx <= sen_idx_max:
+                        sentences.append([])
                         for wrd_idx in line:
-                            self.sentences[sen_idx].append(self.document_vocab[int(wrd_idx)])
+                            sentences[sen_idx - sen_idx_min].append(self.document_vocab[int(wrd_idx)]) # occasionally in the annotation file an article starts at sentence 1 for some reason
         
         # Write output file
         substart = time.time()
-        with open(self.output_path, 'w') as file:
-            file.write('-DOCSTART-          O\n')
-            for sen_idx, sentence in enumerate(self.sentences):
+        with open(output_path, 'a') as file:
+            file.write('\n-DOCSTART-          O\n')
+            for sen_idx, sentence in enumerate(sentences):
                 file.write('\n')
                 for word_idx, word in enumerate(sentence):
                     output = [word]
-                    segments_at_sen_idx = self.segments[sen_idx]
+                    segments_at_sen_idx = segments[sen_idx]
                     have_ne = False
                     for segment in segments_at_sen_idx:
                         begin = segment[0]; end = segment[1]; figer_entity = segment[2]
@@ -158,30 +167,65 @@ class WiFiNECoNLLizer:
                         output.append('O')
                     sep = ''.join([' '] * (20 - len(word)))
                     file.write(sep.join(output) + '\n')
-        print('time to write output file in conllize():', round(time.time() - substart, 2), 'seconds')
-        print('time for conllize():', round(time.time() - start, 2), 'seconds', '\n')
+        # print('time to write output file in conllize():', round(time.time() - substart, 2), 'seconds')
+        # print('time for conllize():', round(time.time() - start, 2), 'seconds', '\n')
+
+
+    def conllize_file(self, annotation_path, output_path):
+        start = time.time()
+        # Read figer annotations
+        art_id = None
+        with open(annotation_path, 'r') as file:
+            for line in file:
+                line = line.split()
+                if (line[0] == 'ID'):
+                    if art_id is not None:
+                        self.conllize_article(annotation_path, output_path, art_id, sen_idx_min, sen_idx_max)
+                    art_id = int(line[1])
+                    sen_idx_min = None
+                    sen_ind_max = None 
+                else:
+                    sen_idx = int(line[0])
+                    if sen_idx_min is None: 
+                        sen_idx_min = sen_idx
+                    sen_idx_max = sen_idx
+            self.conllize_article(annotation_path, output_path, art_id, sen_idx_min, sen_idx_max) # last article in a file
+        print('time for conllize_file():', round(time.time() - start, 2), 'seconds', '\n')
+
+
+    def conllize_all(self):
+        start = time.time()
+        self.load_document_paths()
+        for i in range(82, 100):
+            self.conllize_file(self.annotation_path + str(i), self.output_path + str(i))
+        print('time for conllize_all():', round(time.time() - start, 2), 'seconds', '\n')
 
 
 
 
 #### ------------------------------------------------------------
+NUM_FILES = 3241
 PREFIX = '../WiFiNE_original/'
 annotation_path = PREFIX + 'FineEntity/'
 output_path = '../WiFiNE_CoNLLized/'
 
 
-args = sys.argv[1:]
-try:
-    # Parse argument
-    for i, arg in enumerate(args):
-        if arg == '-a':
-            annotation_path += args[i + 1]
-        elif arg == '-o':
-            output_path += args[i + 1]
-             
-except getopt.error as err:
-    print(err)
+# args = sys.argv[1:]
+# try:
+#     for i, arg in enumerate(args):
+#         if arg == '-a':
+#             annotation_path += args[i + 1]
+#         elif arg == '-o':
+#             output_path += args[i + 1]
 
+#     conllizer = WiFiNECoNLLizer(annotation_path, output_path) 
+#     if (len(args) > 0):
+#         conllizer.conllize()
+#     else:
+#         conllizer.conllize_all()
+             
+# except getopt.error as err:
+#     print(err)
 
 conllizer = WiFiNECoNLLizer(annotation_path, output_path) 
-conllizer.conllize()
+conllizer.conllize_all()
--- src/conllize.sh
+++ src/conllize.sh
@@ -1,8 +1,10 @@
-cd ../WiFiNE_original/FineEntity/
-for file in *
-do
-    cd ../../src
-    echo "CoNLLizing $file..."
-    python conllize.py -a $file -o $file
-    cd ../WiFiNE_original/FineEntity/
-done
+# cd ../WiFiNE_original/FineEntity/
+# for file in *
+# do
+#     cd ../../src
+#     echo "CoNLLizing $file..."
+#     python conllize.py -a $file -o $file
+#     cd ../WiFiNE_original/FineEntity/
+# done
+
+python conllize.py
